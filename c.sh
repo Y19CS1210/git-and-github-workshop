16c5c5d3d8fe4e8524d4329f7346ee24019c1b3b
manikanta <manikantabandla949@gmial.com>
2021-03-19 13:44:06 +0530

    first commit

---
+++ c.sh
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a=10;
+	int b=20;
+	printf("enter a,b values"\n);
+	scanf("%d,&n,%d,&n");
+	sum=a+b;
+	printf("the sum of two numbers"\n);
+	getch();
+}
+}
